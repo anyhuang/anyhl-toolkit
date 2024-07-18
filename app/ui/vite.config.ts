ca4fbe1f5336a75baa887894872fceea39fe9d75
黄某人 <huangjiaqi7701@gmail.com>
2024-07-19 00:16:42 +0800

    🦄 refactor: 重构代码

--- app/ui/vite.config.ts
+++ app/ui/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { URL, fileURLToPath } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
+    ]
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
@@ -15,7 +22,7 @@ export default defineConfig(async () => ({
     strictPort: true,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+      ignored: ['**/core/**', '**/crates/**']
     },
   },
 }));
